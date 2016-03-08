0fbcd252169abf2e8baf675f0b0bb96eec40ad65
Helen Toma <htoma@ucsd.edu>
2016-03-08 11:31:40 -0800

    Got the ports set up. Getting started with the code later

--- i2c_master.vhd
+++ i2c_master.vhd
@@ -1,3 +1,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
+
+entity i2c_master is port (clock	: IN STD_LOGIC;
+			   reset	: IN STD_LOGIC;
+			   data		: IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+			   sda		: INOUT STD_LOGIC;
+			   scl		: OUT STD_LOGIC;
+			   wr		: IN STD_LOGIC);
+end i2c_master;
+
+architecture master_func of i2c_master is
+begin
+end master_func;
--- i2c_slave.vhd
+++ i2c_slave.vhd
@@ -1,3 +1,15 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.numeric_std.ALL;
+
+entity i2c_slave is port (clock		: IN STD_LOGIC;
+			  reset		: IN STD_LOGIC;
+			  data		: OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+			  sda		: INOUT STD_LOGIC;
+			  scl		: IN STD_LOGIC;
+			  rd		: IN STD_LOGIC);
+end i2c_slave;
+
+architecture func of i2c_slave is
+begin
+end func;
